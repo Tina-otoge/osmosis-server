bd94d1a92b1a82682345b20f372a29a3bcfcfc54
Tina <skielred@gmail.com>
2019-12-31 17:50:51 +0100

    api ver bump

--- app/routes/api.py
+++ app/routes/api.py
@@ -10,8 +10,8 @@ from . import dumb_decryption
 @app.route('/versions')
 def versions():
     return jsonify({
-        'osu': 8,
-        'pusher': 4,
+        'osu': 9,
+        'pusher': 5,
     })
 
 
