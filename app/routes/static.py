b07e94c4ec242fb45a1b170db94ee7ddfcc99b42
Tina <me@tina.moe>
2020-03-18 00:33:41 +0100

    About page and plans for the future

--- app/routes/static.py
+++ app/routes/static.py
@@ -6,3 +6,8 @@ from app import app
 @app.route('/tutorial')
 def tutorial():
     return render_template('tutorial.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
