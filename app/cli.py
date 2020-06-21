35340015f9bcdad417a3d2cea64216b1d813318e
Tina <me@tina.moe>
2020-06-22 01:20:06 +0200

    Add command to update song hash easily

--- app/cli.py
+++ app/cli.py
@@ -1,7 +1,8 @@
 import click
 
 from app import db
-from app.ranking import rank_chart, big_button, rescale_charts
+from app.models import Chart
+from app.ranking import rank_chart, big_button, rescale_charts, update_hash
 
 
 def register(app):
@@ -24,3 +25,11 @@ def register(app):
     @click.argument('scale', type=int)
     def rescale(scale):
         rescale_charts(scale)
+
+    @app.cli.command()
+    @click.argument('chart_id', type=int)
+    def uphash(chart_id):
+        chart = Chart.query.get(chart_id)
+        if chart is None:
+            raise Exception('Chart does not exist')
+        update_hash(chart)
